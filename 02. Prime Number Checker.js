d7569f9b74e581adafb2379560bb19971bc0322d
Miroslav Tzarev <mtzarev@gmail.com>
2024-01-20 20:59:53 +0200

    Update 02. Prime Number Checker.js

--- 02. Prime Number Checker.js
+++ 02. Prime Number Checker.js
@@ -1,19 +1,17 @@
 function primeNumberChecker(num) {
-    let isTrue = false;
+    let isPrime = true;
     if (num < 2) {
-        isTrue = false
-    } else {
-        for (let i = 1; i <= num / 2; i++) {
-            if (num % i === 0) {
-                isTrue = false
-            } else {
-                isTrue = true
-            }
+        isPrime = false;
+    }
+    for (let i = 2; i <= num / 2; i++) {
+        if (num % i === 0) {
+            isPrime = false;
+
         }
     }
-    console.log(isTrue);
+    console.log(isPrime);
 }
 
-primeNumberChecker(7);
+//primeNumberChecker(7);
 //primeNumberChecker(8);
-//primeNumberChecker(81);
+primeNumberChecker(81);
